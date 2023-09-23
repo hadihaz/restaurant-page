27131de2b9812bb5825560ec63c91909d8b196db
Hadi <hadihaz1999@gmail.com>
2023-09-24 00:48:13 +0330

    create header

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
-export default function printMe() {
-    console.log('app')
+import './style.css';
+
+function headerGenerate() {
+    const header = document.createElement('div');
+    const logo=document.createElement('h1');
+    const ul=document.createElement('ul');
+    const home=document.createElement('li');
+    const menu=document.createElement('li');
+    const contact=document.createElement('li');
+
+    logo.innerHTML='Restaurant Page';
+    home.innerHTML='Home';
+    menu.innerHTML='Menu';
+    contact.innerHTML='Contact';
+    
+    header.classList.add('header');
+    ul.classList.add('headerList')
+    home.classList.add('headerListItem')
+    menu.classList.add('headerListItem')
+    contact.classList.add('headerListItem')
+
+    home.addEventListener('click',(e)=>{
+        console.log(e.target);
+    })
+    menu.addEventListener('click',(e)=>{
+        console.log(e.target);
+    })
+    contact.addEventListener('click',(e)=>{
+        console.log(e.target);
+    })
+
+    ul.appendChild(home)
+    ul.appendChild(menu)
+    ul.appendChild(contact)
+    header.appendChild(logo);
+    header.appendChild(ul);
+
+    return header;
 }
+
+
+
+export default headerGenerate;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import './style.css';
 // import printMe from './home.js';
 // import printMe from './menu.js';
 // import printMe from './contact.js';
-import printMe from './app.js';
+import headerGenerate from './app.js';
 
 
-console.log("test...!");
 const content =document.getElementById('content')
-const btn=document.createElement('button')
-btn.onclick=printMe;
-content.appendChild(btn)
+
+content.appendChild(headerGenerate())
+
