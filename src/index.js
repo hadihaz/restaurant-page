bbfd443a35d47e4dfa9bff34c10aa2c82d24085b
Hadi <hadihaz1999@gmail.com>
2023-09-23 21:15:26 +0330

    create home.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.css';
+import printMe from './home.js';
+
 
 console.log("test...!");
+const content =document.getElementById('content')
+const btn=document.createElement('button')
+btn.onclick=printMe;
+content.appendChild(btn)
