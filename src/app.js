f593aa1844d664f28dbf4232c1d4af31bf8c3878
Hadi <hadihaz1999@gmail.com>
2023-09-24 17:52:34 +0330

    create footer

--- src/app.js
+++ src/app.js
@@ -2,30 +2,30 @@ import './style.css';
 
 function headerGenerate() {
     const header = document.createElement('div');
-    const logo=document.createElement('h1');
-    const ul=document.createElement('ul');
-    const home=document.createElement('li');
-    const menu=document.createElement('li');
-    const contact=document.createElement('li');
+    const logo = document.createElement('h1');
+    const ul = document.createElement('ul');
+    const home = document.createElement('li');
+    const menu = document.createElement('li');
+    const contact = document.createElement('li');
 
-    logo.innerHTML='Restaurant Page';
-    home.innerHTML='Home';
-    menu.innerHTML='Menu';
-    contact.innerHTML='Contact';
-    
+    logo.innerHTML = 'Restaurant Page';
+    home.innerHTML = 'Home';
+    menu.innerHTML = 'Menu';
+    contact.innerHTML = 'Contact';
+
     header.classList.add('header');
     ul.classList.add('headerList')
     home.classList.add('headerListItem')
     menu.classList.add('headerListItem')
     contact.classList.add('headerListItem')
 
-    home.addEventListener('click',(e)=>{
+    home.addEventListener('click', (e) => {
         console.log(e.target);
     })
-    menu.addEventListener('click',(e)=>{
+    menu.addEventListener('click', (e) => {
         console.log(e.target);
     })
-    contact.addEventListener('click',(e)=>{
+    contact.addEventListener('click', (e) => {
         console.log(e.target);
     })
 
@@ -38,6 +38,26 @@ function headerGenerate() {
     return header;
 }
 
+function footerGenerate() {
+    const footer = document.createElement('div');
+    const p= document.createElement('p');
+    const link= document.createElement('a');
+
+    p.innerHTML='Copyright © 2023';
+    link.innerHTML=' Hadihaz';
+    link.href='https://github.com/hadihaz'
 
 
-export default headerGenerate;
+    footer.classList.add('footer')
+    p.classList.add('footerContent')
+    link.classList.add('footerLink')
+
+    p.appendChild(link);
+    footer.appendChild(p);
+    return footer;
+
+}
+
+
+
+export  {headerGenerate, footerGenerate};
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import './style.css';
 // import printMe from './home.js';
 // import printMe from './menu.js';
 // import printMe from './contact.js';
-import headerGenerate from './app.js';
+import {headerGenerate, footerGenerate} from './app.js';
 
 
 const content =document.getElementById('content')
+const main =document.createElement('div')
+main.classList.add('main')
 
 content.appendChild(headerGenerate())
+content.appendChild(main)
+content.appendChild(footerGenerate())
 
